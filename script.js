d7ecbe31cd31836b4a6f54f178f427a081df19e5
srijagatheeswaran <102983197+srijagatheeswaran@users.noreply.github.com>
2024-05-17 21:10:34 +0530

    Add files via upload
    clear all error

--- script.js
+++ script.js
@@ -1,132 +1,132 @@
-let arr = [];   
-function forSave(){
-    let inputBox = document.getElementById('inputBox');
+let arr = [];
+let actionArr = [];
+let id = [];
+let inputBox = document.getElementById('inputBox');
+let tableBody = document.getElementById('tableBody');
 
-    if(inputBox.value.length > 0){
+inputBox.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("myform").click();
+    }
+  });
+function forSave() {
+    if (inputBox.value.length > 0) {
         document.getElementById('fristTable').classList.add('show');
         document.getElementById('actionBtn').classList.add('show2');
         inputBox.classList.remove('err');
-        var tableBody = document.getElementById('tableBody');
-        tableBody.innerHTML = "";
-        arr.push(inputBox.value);
-        arr.map(item => { 
-            let sno = arr.indexOf(item)+1;
-            let id = arr.indexOf(item);
-            
-        tableBody.innerHTML += ` <tr>
-         <td class="ps-3">${sno}</td>
-         <td class="item">${item}</td>
-         <td class="text-danger">In process</td>
-         <td class="action"><div class="mini"><input type="checkbox" id="checkBox" value = ${id}><i class="bi bi-x h1" id="cancle" onclick= "removeEle(${id})"></i></div></td>
-     
-        </tr>`
-        inputBox.value = ""
         
-         })
+        let exists = arr.some(item => item ==inputBox.value);
+        if(!exists){
+            tableBody.innerHTML = "";
+            arr.push(inputBox.value);
+            arr.map((item, index) => {
+                tableBody.innerHTML += `<tr>
+                    <td class="ps-3">${index + 1}</td>
+                    <td class="item">${item}</td>
+                    <td class="text-danger">In process</td>
+                    <td class="action"><div class="mini"><input type="checkbox" ref="${item}"><i class="bi bi-x h1" onclick="removeEle(${index})"></i></div></td>
+                </tr>`;
+            });
+
+        }
+        else{
+            inputBox.classList.add('err');
+            
+            
+        }
+        
+       
+        inputBox.value = "";
+    } else {
+        inputBox.classList.add('err');
     }
-    else{
-        inputBox.classList.add('err')
-
-    }
- 
-
 }
-function forClear(){
-    inputBox.value = ""
 
+function forClear() {
+    inputBox.value = "";
 }
-function removeEle(element){
-    arr.splice(element,1);
+
+function removeEle(element) {
+    arr.pop(element);
+    let tableBody = document.getElementById('tableBody');
     tableBody.innerHTML = "";
-    if(arr.length ==0){
+    if (arr.length == 0) {
         document.getElementById('fristTable').classList.remove('show');
         document.getElementById('actionBtn').classList.remove('show2');
-
     }
-    arr.map(item => { 
-        let sno = arr.indexOf(item)+1;
-        let id = arr.indexOf(item);
-       
-        
-    document.getElementById('tableBody').innerHTML += ` <tr>
-     <td class="ps-3">${sno}</td>
-     <td class="item">${item}</td>
-     <td class="text-danger">In process</td>
-     <td class="action"><div class="mini"><input type="checkbox" value = ${id}><i class="bi bi-x h1" id="cancle" onclick= "removeEle(${id})"></i></div></td>
- 
-    </tr>`
-    
-    
-})
+    arr.forEach((item, index) => {
+        tableBody.innerHTML += `<tr>
+            <td class="ps-3">${index + 1}</td>
+            <td class="item">${item}</td>
+            <td class="text-danger">In process</td>
+            <td class="action"><div class="mini"><input type="checkbox" ref="${item}"><i class="bi bi-x h1" onclick="removeEle(${index})"></i></div></td>
+        </tr>`;
+    });
 }
 
-let actionArr=[]
-let value
-function saveAction(){
+function saveAction() {
     document.getElementById('table2').classList.add('show');
     let actionTableBody = document.getElementById('actionTableBody');
     let checkBox = document.querySelectorAll('input:checked');
-    console.log(checkBox)
-    actionTableBody.innerHTML ="";
+    actionTableBody.innerHTML = "";
     checkBox.forEach(element => {
-    value = element.getAttribute('value');
-    console.log(element,arr[value])
-    actionArr.push(arr[value])
-    console.log(actionArr)
-    
-});
-actionArr.map((item,index)=> {
-    console.log(item)   
-
-    actionTableBody.innerHTML +=`<tr>
-    <td class="ps-3">${index+1}</td>
-    <td class="item">${item}</td>
-    <td class="text-success mini">Completed <span><i class="bi bi-x h1 text-dark" onclick = "removeEleTable(${value})"></i></span></td>
-</tr>   `
-        arr.splice(value,1);
-        tableBody.innerHTML = "";
-        arr.map(item => { 
-            let sno = arr.indexOf(item)+1;
-            let id = arr.indexOf(item);
-            
-        document.getElementById('tableBody').innerHTML += ` <tr>
-         <td class="ps-3">${sno}</td>
-         <td class="item">${item}</td>
-         <td class="text-danger">In process</td>
-         <td class="action"><div class="mini"><input type="checkbox" id="checkBox" value = ${id}><i class="bi bi-x h1" id="cancle" onclick= "removeEle(${id})"></i></div></td>
-     
-        </tr>`})
+        id.push(element.getAttribute('ref'));
+    });
+    id.forEach(item => {
+        actionArr.push(item);
+        arr.pop(item);
+        
     })
-    if(arr.length ==0){
+    id.splice(0)
+    actionArr.forEach((item, index) => {
+        actionTableBody.innerHTML += `<tr>
+            <td class="ps-3">${index + 1}</td>
+            <td class="item">${item}</td>
+            <td class="text-success ">Completed</td>
+            <td><i class="bi bi-x h1 text-dark removeAll " onclick="removeEleTable(${index})"></i></td>
+        </tr>`;
+    });
+
+    let tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = "";
+    arr.forEach((item, index) => {
+        tableBody.innerHTML += `<tr>
+            <td class="ps-3">${index + 1}</td>
+            <td class="item">${item}</td>
+            <td class="text-danger">In process</td>
+            <td class="action"><div class="mini"><input type="checkbox" ref="${item}"><i class="bi bi-x h1" onclick="removeEle(${index})"></i></div></td>
+        </tr>`;
+    });
+
+    if (arr.length == 0) {
         document.getElementById('fristTable').classList.remove('show');
         document.getElementById('actionBtn').classList.remove('show2');
+    }
+}
 
+function removeEleTable(element) {
+    actionArr.pop(element);
+    let actionTableBody = document.getElementById('actionTableBody');
+    actionTableBody.innerHTML = "";
+    actionArr.forEach((item, index) => {
+        actionTableBody.innerHTML +=`<tr>
+        <td class="ps-3">${index + 1}</td>
+        <td class="item">${item}</td>
+        <td class="text-success ">Completed</td>
+        <td><i class="bi bi-x h1 text-dark removeAll" onclick="removeEleTable(${index})"></i></td>
+    </tr>`;
+    });
+    if (actionArr.length == 0) {
+        document.getElementById('table2').classList.remove('show');
+    }
+}
+function removeAll(){
+    actionArr.splice(0)
+    actionTableBody.innerHTML = "";
+    if (actionArr.length == 0) {
+        document.getElementById('table2').classList.remove('show');
     }
 
-
-    
-}
-function removeEleTable(element){
-    actionArr.splice(element,1);
-    actionTableBody.innerHTML = "";
-    actionArr.map((item,index) => { 
-        let sno = actionArr.indexOf(item)+1;
-        let id = actionArr.indexOf(item);
-        
-    document.getElementById('actionTableBody').innerHTML += `<tr>
-    <td class="ps-3">${index+1}</td>
-    <td class="item">${item}</td>
-    <td class="text-success mini">Completed <span><i class="bi bi-x h1 text-dark" onclick = "removeEleTable(${value})"></i></span></td>
-</tr>`
-
-    
-})
-if(actionArr.length ==0){
-    document.getElementById('table2').classList.remove('show');
-    
-}
-else{
-    
-}
 
 }
